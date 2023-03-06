d9804fcaa9346590493d19bcc4718778b121bc2d
Nico <contact@craftnut.wtf>
2023-03-05 22:01:19 -0500

    Split preview and save.
    Split preview and save, also added code comments to help make code understandable.

--- gui.py
+++ gui.py
@@ -3,62 +3,64 @@ import sys
 import numpy
 from PIL import Image
 from pixelsorter import pixelsort
-from ctypes import alignment
-from PySide6.QtGui import QPixmap
-from PySide6.QtWidgets import QMainWindow, QApplication, QLabel, QPushButton, QLineEdit, QDialog, QGridLayout, QWidget
+from PySide6.QtWidgets import QApplication, QLabel, QPushButton, QLineEdit, QDialog, QGridLayout, QWidget
 from PySide6 import QtCore
 
-
-# img = cv2.imread('test.png')
-diff:int = 0
-
-
+# Setup the QWigdet and other stuff
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Pixelsorter GUI")
         
+        # Image directory input
         self.imgdir = QLineEdit(self)
         imglabel = QLabel("img dir: ")
+        
+        # Difference input
         self.pxvalue = QLineEdit(self)
         pxlabel = QLabel("difference: ")
         
-        go = QPushButton("go")
-        go.clicked.connect(self.go_clicked)
+        # Preview button setup
+        preview_button = QPushButton("Preview")
+        preview_button.clicked.connect(self.preview_clicked)
         
+        # Save button setup
+        save_button = QPushButton("Save")
+        save_button.clicked.connect(self.save_clicked)
+        
+        # Create a grid layout with all the above 
         layout = QGridLayout(self)
-        layout.addWidget(go, 0, 0, 1, 1)
+        layout.addWidget(preview_button, 0, 0, 1, 1)
+        layout.addWidget(save_button, 0, 1, 1, 1)
         layout.addWidget(pxlabel, 1, 0, 1, 1)
         layout.addWidget(self.pxvalue, 1, 1, 1, 1)
         layout.addWidget(imglabel, 2, 0, 1, 1)
         layout.addWidget(self.imgdir, 2, 1, 1, 1)
-        
-    # class Image(QApplication):
-    #     def __init__(self):
-    #         super().__init__()
-    #         label = QLabel(self)
-    #         pixmap = QPixmap('sorted.png')
-    #         label.setPixmap(pixmap)
-    #         self.setWindowTitle("test show")
-    #         self.setCentralWidget(label)
-    #         self.resize(pixmap.width(), pixmap.height())
-            
+
     @QtCore.Slot()
     
-    def go_clicked(self):
+    # Preview button clicked functionality
+    def preview_clicked(self):
         
+        # Generate and display the pixelsort
         dir = self.imgdir.text()
         diff = self.pxvalue.text()
-        
         cv2.destroyAllWindows()
         img = cv2.imread(str(dir))
         pxsorted = pixelsort(img, int(diff))
         cv2.imshow('CV2, check folder for sorted.png', pxsorted)
-        cv2.imwrite('sorted.png', pxsorted)
-        # show = MainWindow.Image()
-        # show.exec()
+    
+    # Save button clicked functionality
+    def save_clicked(self):
         
+        # Generate and save the pixelsort to 'sorted.png' REMEMBER TO SET USER SAVE LATER!!!
+        dir = self.imgdir.text()
+        diff = self.pxvalue.text()
+        img = cv2.imread(str(dir))
+        pxsorted = pixelsort(img, int(diff))
+        cv2.imwrite('sorted.png', pxsorted)
 
+# Launch the QWidget
 app = QApplication(sys.argv)
 mw = MainWindow()
 mw.resize(280, 110)
